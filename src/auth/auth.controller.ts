514b6655bc7222de44f29837d319ed16b6ba044d
Arthur Roberto Alves <arthurrobertoalves@gmail.com>
2026-03-07 22:35:21 -0300

    fix(JWT):Cookies being stored and validated.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,33 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Res,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import type { Response } from 'express';
 import { LoginDto } from './dto/login.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
-    @HttpCode(HttpStatus.OK)
-    @Post('login')
-    login(@Body() loginDto: LoginDto)  {
-        return this.authService.login(loginDto);
-    }
+  @HttpCode(HttpStatus.OK)
+@Post("login")
+async login(
+  @Body() loginDto: LoginDto,
+  @Res({ passthrough: true }) res: Response
+) {
+  const result = await this.authService.login(loginDto);
+
+  res.cookie("token", result.access_token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
+
+  return { success: true };
+}
 }
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://arbon-hr.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
