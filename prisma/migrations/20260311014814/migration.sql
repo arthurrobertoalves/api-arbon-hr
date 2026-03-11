298c1e3f41e533d16efdbbcf8870fb4b3a134a4d
Arthur Roberto Alves <arthurrobertoalves@gmail.com>
2026-03-11 00:09:21 -0300

    feat: add companies management page to admin dashboard

---
+++ prisma/migrations/20260311014814/migration.sql
@@ -0,0 +1,44 @@
+/*
+  Warnings:
+
+  - The values [EMPLOYEE] on the enum `UserRole` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "UserRole_new" AS ENUM ('ADMIN', 'CUSTOMER');
+ALTER TABLE "public"."User" ALTER COLUMN "role" DROP DEFAULT;
+ALTER TABLE "User" ALTER COLUMN "role" TYPE "UserRole_new" USING ("role"::text::"UserRole_new");
+ALTER TYPE "UserRole" RENAME TO "UserRole_old";
+ALTER TYPE "UserRole_new" RENAME TO "UserRole";
+DROP TYPE "public"."UserRole_old";
+ALTER TABLE "User" ALTER COLUMN "role" SET DEFAULT 'CUSTOMER';
+COMMIT;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "companyId" TEXT;
+
+-- CreateTable
+CREATE TABLE "Company" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Company_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Participant" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "companyId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Participant_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Participant" ADD CONSTRAINT "Participant_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -3,4 +3,6 @@ import { UserRole } from "generated/prisma/enums";
 export class CreateUserDto {
   role: UserRole;
   code: string;
+  companyId?: string;
+  company?: string;
 }
